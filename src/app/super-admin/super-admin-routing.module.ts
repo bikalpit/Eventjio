bed3197609aa977ec009079e9563e272660e634b
narinderahuja79 <bikalpitbhadani@gmail.com>
2020-08-20 15:07:40 +0530

    Kalpit

--- src/app/super-admin/super-admin-routing.module.ts
+++ src/app/super-admin/super-admin-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MySubscriptionsComponent } from './my-subscriptions/my-subscriptions.component';
 
 const routes: Routes = [
   { 
     path: '', 
     component: DashboardComponent
+  },
+  { 
+    path: 'my-subscriptions', 
+    component: MySubscriptionsComponent
   }
   ];
 
--- src/app/super-admin/super-admin.module.ts
+++ src/app/super-admin/super-admin.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MySubscriptionsComponent } from './my-subscriptions/my-subscriptions.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, MySubscriptionsComponent],
   imports: [
     CommonModule,
   ]
