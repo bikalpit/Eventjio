6f26beea944947cc064f0718889d7c02a6f66fa6
narinderahuja79 <bikalpitbhadani@gmail.com>
2020-08-20 15:08:37 +0530

    Kalpit

--- src/app/super-admin/super-admin.module.ts
+++ src/app/super-admin/super-admin.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { MySubscriptionsComponent } from './my-subscriptions/my-subscriptions.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent, MySubscriptionsComponent],
+  declarations: [DashboardComponent],
   imports: [
     CommonModule,
   ]
