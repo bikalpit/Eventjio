99fa5bbe905739ac0c913e155e89792704248644
Jignesh Surati <jignesh@broadviewinnovations.in>
2020-08-21 18:31:00 +0530

    Jignesh

--- src/app/super-admin/events/events.component.ts
+++ src/app/super-admin/events/events.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+
 
 @Component({
   selector: 'app-events',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events.component.scss']
 })
 export class EventsComponent implements OnInit {
+  displayedColumns: string[] = ['Event','Status','Sold','Remaining','Revenue','Togglebtn'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
   }
 
+  
+
 }
+export interface PeriodicElement {
+  Event: string;
+  Status: string;
+  Sold: string;
+  Remaining: string;
+  Revenue:string;
+  Togglebtn:string;
+} 
+
+const ELEMENT_DATA: PeriodicElement[] = [
+{Event:'test',Status:'Waiting',Sold:'00',Remaining:'00',Revenue:'00',Togglebtn:''}
+  
+];
--- src/app/super-admin/super-admin.module.ts
+++ src/app/super-admin/super-admin.module.ts
@@ -5,11 +5,12 @@ import { SuperAdminRoutingModule } from './super-admin-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EventsComponent } from './events/events.component';
 import { MaterialModule } from '../_helpers/material.module';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [DashboardComponent, EventsComponent],
   imports: [
-    CommonModule, SuperAdminRoutingModule, MaterialModule
+    CommonModule, SuperAdminRoutingModule, MaterialModule, MatTableModule
   ]
 })
 export class SuperAdminModule { }
