baa468a4ef6317a0c22ced728c15f5d6e50068a8
Jignesh Surati <jignesh@broadviewinnovations.in>
2020-08-27 13:01:53 +0530

    jignesh

--- src/app/super-admin/settings/settings-routing.module.ts
+++ src/app/super-admin/settings/settings-routing.module.ts
@@ -6,7 +6,9 @@ import { SettingsComponent } from './settings.component';
 const routes: Routes = [{ path: '', component: SettingsComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [
+    RouterModule.forChild(routes)
+  ],
   exports: [RouterModule]
 })
 export class SettingsRoutingModule { }
