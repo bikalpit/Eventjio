623750ca10210370dd1c734995b2debc058f961c
narinderahuja79 <bikalpitbhadani@gmail.com>
2020-08-27 11:35:05 +0530

    Kalpit

--- src/app/super-admin/super-admin-routing.module.ts
+++ src/app/super-admin/super-admin-routing.module.ts
@@ -17,7 +17,9 @@ const routes: Routes = [
   { 
     path: 'orders', 
     component: OrdersComponent
-  }
+  },
+  { path: 'settings', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) },
+  
   ];
 
 @NgModule({
