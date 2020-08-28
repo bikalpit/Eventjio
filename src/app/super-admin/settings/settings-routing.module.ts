04aba591633d3c0fafcbe30e08e1ae8c71f5d81d
narinderahuja79 <bikalpitbhadani@gmail.com>
2020-08-28 14:41:19 +0530

    Kalpit

--- src/app/super-admin/settings/settings-routing.module.ts
+++ src/app/super-admin/settings/settings-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { SettingsComponent } from './settings.component';
 
-const routes: Routes = [{ path: '', component: SettingsComponent }];
+const routes: Routes = [];
 
 @NgModule({
   imports: [
