298ffbca97c4ca9b322800ca82ac2c4cc651421b
narinderahuja79 <narinderahuja79@gmail.com>
2020-09-04 18:30:20 +0530

    Jignesh

--- src/app/super-admin/events/events.component.ts
+++ src/app/super-admin/events/events.component.ts
@@ -14,23 +14,27 @@ interface Status {
 })
 export class EventsComponent implements OnInit {
   
-  clickedIndex:any = 'coupon'
+ addNewEvents : boolean = true;
+  
   upcomingEventData = [{event:'Lajawab Cooking Classes',status:'Draft',sold:'00',remaining:'00',revenue:'$.00.00',togglebtn:''},
                 {event:'Draculla Drinks',status:'Published',sold:'20',remaining:'200',revenue:'$.2000.00',togglebtn:''},
                 {event:'Draculla Drinks',status:'Published',sold:'20',remaining:'200',revenue:'$.2000.00',togglebtn:''},]
-  pastEventData = [{event:'Lajawab Cooking Classes',status:'Draft',sold:'00',remaining:'00',revenue:'$.00.00',togglebtn:''},
+ 
+                pastEventData = [{event:'Lajawab Cooking Classes',status:'Draft',sold:'00',remaining:'00',revenue:'$.00.00',togglebtn:''},
                 {event:'Draculla Drinks',status:'Published',sold:'20',remaining:'200',revenue:'$.2000.00',togglebtn:''},
                 ]
+
+  
   constructor() { }
+
+  fnAddNewEventsForm(){
+    this.addNewEvents = !this.addNewEvents;
+  }
 
   ngOnInit(): void {
     
   }
 
-  Status: Status[] = [
-    {value: 'Draft', viewValue: 'Draft'},
-    {value: 'Published', viewValue: 'Published'},
-
-  ];  
+ 
 
 }
