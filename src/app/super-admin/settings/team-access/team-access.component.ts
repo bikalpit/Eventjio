f830e1f59b038190791d21f61edc213c8ca6ffce
bikalpit <60174681+bikalpit@users.noreply.github.com>
2020-09-10 16:52:41 +0530

    unnati

--- src/app/super-admin/settings/team-access/team-access.component.ts
+++ src/app/super-admin/settings/team-access/team-access.component.ts
@@ -6,9 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./team-access.component.scss']
 })
 export class TeamAccessComponent implements OnInit {
-  teammates = [{name:'unnati',role:'manager',email:'abc@gmail.com',lastseen:'14 Jul 2020 4:30 pm'},
-  {name:'unnati',role:'manager',email:'abc@gmail.com',lastseen:'14 Jul 2020 4:30 pm'},
-  {name:'unnati',role:'manager',email:'abc@gmail.com',lastseen:'14 Jul 2020 4:30 pm'}
+
+  teammates = [{name:'Meet Shah',role:'Owner',email:'abc@gmail.com',lastseen:'14 Jul 2020 4:30 pm'},
+  {name:'Monoj Tiwari',role:'Event Manager',email:'abc@gmail.com',lastseen:'14 Jul 2020 4:30 pm'},
+  {name:'Parth Bishnoi',role:'Order Manager',email:'abc@gmail.com',lastseen:'14 Jul 2020 4:30 pm'}
+]
+
+ invite = [{email:'shah.meet@gmail.com',roles:'Order manager',editrole:'Order manager with edit',exportrole:'Order manager with export',lastseen:'Jul 14, 2020 4:44 pm',status:'Pending'},
 ]
 
 
