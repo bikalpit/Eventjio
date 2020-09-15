55a71c6ed416e291ab7018c45231acbea0764d3a
narinderahuja79 <bikalpitbhadani@gmail.com>
2020-09-15 12:52:08 +0530

    Kalpit
    Login Form Validation Setup

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm:FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { 
+    let emailPattern=/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/
+    this.loginForm = this.formBuilder.group({
+        email: ['',[Validators.required,Validators.email,Validators.pattern(emailPattern)]],
+        password: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
   }
