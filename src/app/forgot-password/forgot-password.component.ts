bde7364595260f1e7d85f7ff3b32adf15a368ed0
nikhil834783 <70576740+nikhil834783@users.noreply.github.com>
2020-09-16 12:49:11 +0530

    changes by nikhil
    signup-api,login-api

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient, HttpErrorResponse, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-forgot-password',
@@ -8,8 +13,20 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ForgotPasswordComponent implements OnInit {
   forgotForm:FormGroup;
+  dataLoaded: boolean = true;
+   forgotPwdContainer: boolean = true;
+   emailSentContainer: boolean = false;
+   forgotEmail: any;
+   error = '';
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private http:HttpClient,
+    private route: ActivatedRoute,
+    private router: Router,       
+    private _snackBar: MatSnackBar,
+  
+    ) {
     let emailPattern=/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/
     this.forgotForm = this.formBuilder.group({
         email: ['',[Validators.required,Validators.email,Validators.pattern(emailPattern)]],
@@ -19,4 +36,57 @@ export class ForgotPasswordComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  private handleError(error: HttpErrorResponse) {
+    console.log(error);
+    return throwError('Error! something went wrong.');
+}
+
+
+forgotPwdSubmit(){
+  this.forgotEmail =  this.forgotForm.get('email').value
+  if(this.forgotForm.valid){
+    // let site_url = login.urlForLink;
+  let requestObject = {
+        "email":this.forgotEmail,
+        // "url" : site_url+"/reset-password?accessToken"
+      };
+  let headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+      });
+      return this.http.post(`/forgot-password`,requestObject,{headers:headers}).pipe(
+      map((res) => {
+          return res;
+      }),
+      catchError(this.handleError)
+      ).subscribe((response:any) => {
+        if(response.data == true){
+          // this._snackBar.open("Reset password link sent in your mail", "X", {
+          //   duration: 2000,
+          //   verticalPosition:'top',
+          //   panelClass :['green-snackbar']
+          // });
+          this.forgotPwdContainer =false
+          this.emailSentContainer = true;
+          setTimeout(() => {
+            this.router.navigate(['/login']);
+          }, 4000);
+        }
+        else if(response.data == false){
+          this._snackBar.open(response.response, "X", {
+            duration: 2000,
+            verticalPosition:'top',
+            panelClass :['red-snackbar']
+          });
+        }
+      }, (err) =>{
+        console.log(err)
+      })
+  }else{
+   this.forgotForm.get('email').markAsTouched();
+  }
+  
+}
+fnCloseForgotPwd(){
+  this.router.navigate(['/login']);
+}
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthenticationService } from '../_services/authentication.service';
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,17 +13,89 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   loginForm:FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error = '';
+  hide = true;
+  hideLoginForm: boolean = true;
+  dataLoaded: boolean = false;
+  isIE: boolean = false;
+
   constructor(
     private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private _snackBar: MatSnackBar,
+    private authenticationService: AuthenticationService,
+    //private appComponent:AppComponent,
   ) { 
+    if(/msie\s|trident\//i.test(window.navigator.userAgent)){
+            this.isIE = true;
+  }
+  };
+
+  ngOnInit(): void {
     let emailPattern=/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/
     this.loginForm = this.formBuilder.group({
         email: ['',[Validators.required,Validators.email,Validators.pattern(emailPattern)]],
         password: ['', Validators.required]
     });
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
+  get f() { return this.loginForm.controls; }
 
-  ngOnInit(): void {
-  }
+  onSubmit() {
+    this.submitted = true;
+    if(this.loginForm.invalid){
+        this.loginForm.get('email').markAsTouched();
+        this.loginForm.get('password').markAsTouched();
 
+        return false;
+    }
+    this.authenticationService.login(this.loginForm.get('email').value, this.loginForm.get('password').value)
+    .pipe(first()).subscribe(data => {
+
+        if(data.data == true){
+          
+            if(data.response.currentPlan == null){
+                localStorage.setItem('adminData',JSON.stringify(data.response))
+                localStorage.removeItem('currentUser');
+                this.router.navigate(["/super-admin"]);
+                return;    
+            }
+
+            if(data.response.user_type == "A"){
+                this.router.navigate(["admin"]);
+            }else if(data.response.user_type == "SM"){
+                localStorage.setItem('internal_staff','N');
+                this.router.navigate(["staff"]);
+            }else{
+                this.router.navigate(["user"]);
+            }
+
+            // this.appComponent.initiateTimeout();
+            this.hideLoginForm = false;
+            
+        }else if(data.data == false){
+
+            this._snackBar.open(data.response, "X", {
+                duration: 2000,
+                verticalPosition:'top',
+                panelClass :['red-snackbar']
+                });
+            this.error = data.response; 
+            this.dataLoaded = true;
+
+        }  else{
+            this.error = "Database Connection Error."; 
+            this.dataLoaded = true;
+        }
+
+    },
+    error => {  
+        this.error = "Database Connection Error."; 
+        this.dataLoaded = true;  
+    });
+}
 }
