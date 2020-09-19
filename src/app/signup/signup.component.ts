af1775ff30bfd12173dc9ec7ac42f5a534842282
bikalpit <60174681+bikalpit@users.noreply.github.com>
2020-09-19 16:23:14 +0530

    unnati

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -18,7 +18,6 @@ export class SignupComponent implements OnInit {
   signUpForm: FormGroup;
   hide = true;
   adminSignUpData:any;
-  hide = true;
   termsCheckbox:boolean = false;
 
 
@@ -57,16 +56,19 @@ export class SignupComponent implements OnInit {
 
   fnSignUp(){
     if(!this.termsCheckbox){
+		alert("1")
       return false;
     }
     
     if(this.signUpForm.invalid){
+		alert("2")
       this.signUpForm.get('firstname').markAsTouched;
       this.signUpForm.get('email').markAsTouched;
       this.signUpForm.get('password').markAsTouched;
       this.signUpForm.get('description').markAsTouched;
       return false;
     }
+	alert("3")
 
       let requestObject = {
 				"firstname":this.signUpForm.get("firstname").value,
