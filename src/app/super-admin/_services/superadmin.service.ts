669c0c9d4a81913a6b7ae73ae54c8c8674ce6a24
Kalpit Bhadani <bikalpitbhadani@gmail.com>
2020-10-16 11:40:00 +0530

    Kalpit

--- src/app/super-admin/_services/superadmin.service.ts
+++ src/app/super-admin/_services/superadmin.service.ts
@@ -263,26 +263,18 @@ export class SuperadminService {
             return res;
         }),catchError(this.handleError));
     }
-<<<<<<< Updated upstream
     fnAssignEventToVoucher(requestObject){
-=======
-    
-    fnAssignToEvent(requestObject){
->>>>>>> Stashed changes
         return this.http.post(`${environment.apiUrl}/assign-voucher-to-event`,requestObject,{headers:this.globalHeaders}).pipe(
         map((res) => {
             return res;
         }),catchError(this.handleError));
     }
-<<<<<<< Updated upstream
     fnAssignTicketToCoupon(requestObject){
         return this.http.post(`${environment.apiUrl}/coupon-apply-to-ticket`,requestObject,{headers:this.globalHeaders}).pipe(
         map((res) => {
             return res;
         }),catchError(this.handleError));
     }
-=======
->>>>>>> Stashed changes
 
 }
 
