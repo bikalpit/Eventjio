62a136a5fa61ba4594b35c2eef69bf9b1d92d910
bijayesh6 <bijayesh6@gmail.com>
2020-11-21 15:23:46 +0530

    working on APi

--- src/app/super-admin/single-event-dashboard/duplicate/duplicate.component.ts
+++ src/app/super-admin/single-event-dashboard/duplicate/duplicate.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {SingleEventServiceService} from '../_services/single-event-service.service';
+import { SingleEventServiceService } from '../_services/single-event-service.service';
 import { ErrorService } from '../../../_services/error.service';
-import { DatePipe} from '@angular/common';
+import { DatePipe } from '@angular/common';
 import { Router, ActivatedRoute } from '@angular/router';
 
 
@@ -16,101 +16,144 @@ export class DuplicateComponent implements OnInit {
 
   duplicateArray = [];
   items: FormArray;
-  duplicateForm:FormGroup;
+  duplicateForm: FormGroup;
   minDate = new Date();
-  minTillDate :any = new Date();
-  isLoaderAdmin:boolean = false;
-  duplicateId:any;
-  eventName:any;
+  minTillDate: any = new Date();
+  isLoaderAdmin: boolean = false;
+  duplicateId: any;
+  eventName: any;
   // duplicateForm:boolean=false;
 
   constructor(
-    private formBuilder:FormBuilder,
-    private SingleEventServiceService:SingleEventServiceService,
-    private ErrorService:ErrorService,
-    private datePipe:DatePipe,
+    private formBuilder: FormBuilder,
+    private SingleEventServiceService: SingleEventServiceService,
+    private ErrorService: ErrorService,
+    private datePipe: DatePipe,
     private router: Router,
-  ) 
-  { 
+  ) {
     this.duplicateArray.length = 1;
-    if(localStorage.getItem('selectedEventCode')){
+    if (localStorage.getItem('selectedEventCode')) {
       this.duplicateId = localStorage.getItem('selectedEventCode')
     }
-  
+
+    // this.duplicateForm = this.formBuilder.group({
+    //   event_title: ['', Validators.required],
+    //   start_date: ['', Validators.required],
+    //   end_date: ['', Validators.required],
+    //   event_status: ['', Validators.required],
+    // });
+
     this.duplicateForm = this.formBuilder.group({
-      event_title:['',Validators.required],
-      start_date:['',Validators.required],
-      end_date:['',Validators.required],
-      event_status:['',Validators.required],
-   });
-          
+      items: this.formBuilder.array([ this.createItem() ])
+    });
+
   }
 
   ngOnInit(): void {
-    this. fnGetEventDetail();
-  }
-  refresh(): void {
-    window.location.reload();
-}
-  
-  fnAddDuplicate(){
-    this.duplicateArray.push(this.duplicateArray.length+1);
-  }
-  
-  fnRemoveDuplicate(index){
-    this.duplicateArray.splice(index,1)
+    this.fnGetEventDetail();
   }
 
-  fnChangeValideFrom(){
-    this.minTillDate =this.duplicateForm.get('start_date').value;
-    this.duplicateForm.get('end_date').setValue('');
+  createItem(): FormGroup {
+    return this.formBuilder.group({
+      event_title: ['',Validators.required],
+      start_date:  ['',Validators.required],
+      end_date:  ['',Validators.required],
+      event_status:  ['',Validators.required]
+    });
   }
 
-  fnGetEventDetail(){
+
+  fnAddDuplicate() {
+    this.items = this.duplicateForm.get('items') as FormArray;
+    this.items.push(this.createItem());
+  }
+
+  fnRemoveDuplicate(index) {
+    var x = confirm('Are you sure you want to delete?')
+    if(x){
+      (this.duplicateForm.get('items') as FormArray).removeAt(index);
+    }
+  }
+
+
+
+  getContactsFormGroup(index): FormGroup {
+    return this.duplicateForm.controls[index] as FormGroup;
+  }
+
+  fnGetEventDetail() {
 
     let requestObject = {
-      'unique_code' :this.duplicateId,
+      'unique_code': this.duplicateId,
     }
 
-    this.SingleEventServiceService.getSingleEvent(requestObject).subscribe((response:any) => {
-      if(response.data == true){
+    this.SingleEventServiceService.getSingleEvent(requestObject).subscribe((response: any) => {
+      if (response.data == true) {
         this.eventName = response.response.event[0];
-        this.eventName.start_date =  this.datePipe.transform(this.eventName.start_date,"EEE MMM d, y")
-        this.eventName.end_date =  this.datePipe.transform(this.eventName.end_date,"EEE MMM d, y")
-        console.log(this.eventName);
-        // this.eventStatus = this.eventDetail.event_status;
-      } else if(response.data == false){
+        this.eventName.start_date = this.datePipe.transform(this.eventName.start_date, "EEE MMM d, y")
+        this.eventName.end_date = this.datePipe.transform(this.eventName.end_date, "EEE MMM d, y")
+      } else if (response.data == false) {
         this.ErrorService.errorMessage(response.response);
       }
     });
 
   }
 
-  saveDuplicate(){
-    if(this.duplicateForm.invalid){
-      this.duplicateForm.get('event_title').markAsTouched();
-      this.duplicateForm.get('start_date').markAsTouched();
-      this.duplicateForm.get('end_date').markAsTouched();
-      this.duplicateForm.get('event_status').markAsTouched();
+  async saveDuplicate() {
+
+    if (this.duplicateForm.invalid) {
+      // this.duplicateForm.get('event_title').markAsTouched();
+      // this.duplicateForm.get('start_date').markAsTouched();
+      // this.duplicateForm.get('end_date').markAsTouched();
+      // this.duplicateForm.get('event_status').markAsTouched();
+      this.ErrorService.errorMessage('please fill up the required fields');
       return false;
     }
-    this.isLoaderAdmin = true;
-    let requestObject ={
-      "unique_code":this.duplicateId,
-      "start_date":this.datePipe.transform(new Date(this.duplicateForm.get('start_date').value),"yyyy-MM-dd"),
-      "end_date":this.datePipe.transform(new Date(this.duplicateForm.get('end_date').value),"yyyy-MM-dd"),
-      "event_title":this.duplicateForm.get("event_title").value,
-      "event_status":this.duplicateForm.get("event_status").value,
+
+    var is_wrong_date_select = false;
+    this.duplicateForm.value.items.forEach((element,aa,key) => {
+      var start_date = this.datePipe.transform(new Date(element.start_date), "yyyy-MM-dd");
+      var end_date = this.datePipe.transform(new Date(element.end_date), "yyyy-MM-dd");
+
+      var g1 = new Date(start_date); 
+      var g2 = new Date(end_date);
+     
+      if (g1.getTime() >  g2.getTime()) {
+        is_wrong_date_select = true;
+      }
+    });
+
+    if(is_wrong_date_select){
+      this.ErrorService.errorMessage('please select valid date of events');
+      return;
     }
-  this.SingleEventServiceService.duplicateForm(requestObject).subscribe((response:any)=>{
-  if(response.data == true){
-    this.ErrorService.successMessage(response.response);
-    this.router.navigate(["/super-admin/events"]);
     
-} else if(response.data == false){
-  this.ErrorService.errorMessage(response.response);
-  }
-  this.isLoaderAdmin = false;
-});
+    var i = 0;
+    await this.duplicateForm.value.items.forEach(element => {
+
+      let requestObject = {
+        "start_date" : this.datePipe.transform(new Date(element.start_date), "yyyy-MM-dd"),
+        "end_date" : this.datePipe.transform(new Date(element.end_date), "yyyy-MM-dd"),
+        "event_title" : element.event_title,
+        "event_status" : element.event_status,
+        "unique_code": this.duplicateId,
+      };
+      this.isLoaderAdmin = true;
+      i++;
+
+       this.SingleEventServiceService.duplicateForm(requestObject).subscribe((response: any) => {
+        if (response.data == true) {
+          this.ErrorService.successMessage(response.response);
+          if(this.duplicateForm.value.items.length == i){
+            this.router.navigate(["/super-admin/events"]);
+          }
+        } else if (response.data == false) {
+          this.ErrorService.errorMessage(response.response);
+        }
+        this.isLoaderAdmin = false;
+      });
+
+    });
+   
   }
 }
