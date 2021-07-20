554f55238eb642080b945a43aefbbc72df0d2139
Kalpit <bikalpitbhadani@gmail.com>
2021-07-20 12:45:01 +0530

    Changes given by Suhas sir

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators,FormControl } from '@angular/forms';
+import { first } from 'rxjs/operators';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import { Router, RouterOutlet ,ActivatedRoute} from '@angular/router';
 import { HttpClient, HttpErrorResponse, HttpParams, HttpHeaders } from '@angular/common/http';
@@ -9,6 +10,7 @@ import { Observable, throwError } from 'rxjs';
 import { AuthenticationService } from '../_services/authentication.service';
 import { ErrorService } from '../_services/error.service'
 import { AppComponent } from '../app.component';
+import { AuthenticationService } from '../_services/authentication.service';
 
 @Component({
   selector: 'app-signup',
@@ -27,6 +29,8 @@ export class SignupComponent implements OnInit {
   requestObject:any;
   hide= true;
   hide1 = true;
+  error = '';
+  keepMe: any = 'false';
 
   constructor( private formBuilder: FormBuilder,
   private http: HttpClient,
@@ -34,6 +38,7 @@ export class SignupComponent implements OnInit {
   private route: ActivatedRoute,
   private _snackBar: MatSnackBar,
   private ErrorService: ErrorService,
+  private authenticationService: AuthenticationService,
   private appComponent: AppComponent,
     )
      {
@@ -138,7 +143,8 @@ export class SignupComponent implements OnInit {
 					panelClass : ['green-snackbar']
 				});
 				this.isLoaderAdmin = false;
-				this.router.navigate(["/login"]);
+				this.onLogin();
+				// this.router.navigate(["/login"]);
 			}else{
 				this.ErrorService.errorMessage(response.response);
 			
@@ -149,10 +155,93 @@ export class SignupComponent implements OnInit {
 		(err) =>{
 			console.log(err)
 		})
-
-
     
   }
+
+  
+  onLogin() {
+
+	this.isLoaderAdmin = true;
+
+	this.authenticationService.login(this.signUpForm.get('email').value, this.signUpForm.get('ReNewPassword').value)
+		.pipe(first()).subscribe(data => {
+
+			if (data.data == true) {
+				// alert(this.keepMe)
+				if (this.keepMe == 'true') {
+					// alert('localStorage')
+					localStorage.setItem('currentUser', JSON.stringify(data.response))
+				} else {
+					sessionStorage.setItem('currentUser', JSON.stringify(data.response))
+				}
+
+				let currentUser = data.response;
+				if (currentUser.type == 'member') {
+					localStorage.setItem('isBoxoffice', 'false');
+					localStorage.setItem('boxoffice_id', currentUser.boxoffice_id);
+					localStorage.setItem('boxoffice_name', currentUser.boxoffice_name);
+
+					if (currentUser.permission != 'A') {
+						
+						localStorage.removeItem('permision_OM');
+						localStorage.removeItem('permision_OV');
+						localStorage.removeItem('permision_EM');
+						var permistion = currentUser.permission.split(",");
+
+						if (permistion.indexOf("EM") > -1) {
+							localStorage.setItem('permision_EM', 'TRUE');
+							// localStorage.removeItem('permision_OM');
+							// localStorage.removeItem('permision_OV');
+							localStorage.removeItem('permision_ALL');
+
+						}
+
+						if (permistion.indexOf("OM") > -1) {
+							localStorage.setItem('permision_OM', 'TRUE');
+							// localStorage.removeItem('permision_EM');
+							// localStorage.removeItem('permision_OV');
+							localStorage.removeItem('permision_ALL');
+						}
+
+						if (permistion.indexOf("OV") > -1) {
+							localStorage.setItem('permision_OV', 'TRUE');
+							// localStorage.removeItem('permision_OM');
+							// localStorage.removeItem('permision_EM');
+							localStorage.removeItem('permision_ALL');
+						}
+
+					} else {
+						localStorage.setItem('permision_ALL', 'TRUE');
+						localStorage.removeItem('permision_OM');
+						localStorage.removeItem('permision_EM');
+						localStorage.removeItem('permision_OV');
+					}
+
+				}
+				this.router.navigate(["/super-admin"]);
+				// this.isLoaderAdmin = false;
+
+			} else if (data.data == false) {
+
+				this._snackBar.open(data.response, "X", {
+					duration: 2000,
+					verticalPosition: 'top',
+					panelClass: ['red-snackbar']
+				});
+				this.error = data.response;
+				this.dataLoaded = true;
+
+			} else {
+				this.error = "Database Connection Error.";
+			}
+			this.isLoaderAdmin = false;
+		}, error => {
+			this.error = "Database Connection Error.";
+			this.dataLoaded = true;
+		});
+
+}
+
   isEmailUnique(control: FormControl) {
 		return new Promise((resolve, reject) => {
 		  setTimeout(() => {
--- src/app/super-admin/settings/settings-routing.module.ts
+++ src/app/super-admin/settings/settings-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [{
                   },
                   {
                     path:'event-page-design',
-                    component:ButttonAndLinksComponent
+                    component:EventPageDesignComponent
                   },
 
                   {
