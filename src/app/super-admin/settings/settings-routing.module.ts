85fe07715f5dfb73efc3876946f56235db407ce6
Kalpit <bikalpitbhadani@gmail.com>
2021-07-21 10:00:18 +0530

    update Event design page view

--- src/app/super-admin/settings/settings-routing.module.ts
+++ src/app/super-admin/settings/settings-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [{
               component: SettingsComponent, 
               children:[{
                     path:'',
-                    component:EventPageDesignComponent
+                    component:WebsitesEmbedCodesComponent
                   },
                   {
                     path:'event-page-design',
